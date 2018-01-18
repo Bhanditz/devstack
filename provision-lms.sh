06fa4cd2a230525db329a1a52d0cc3232e3812f4
Gregory Martin <greg@edx.org>
2018-01-18 15:50:57 -0500

    Update

--- provision-lms.sh
+++ provision-lms.sh
@@ -16,7 +16,7 @@ done
 DIR="$( cd "$( dirname "$( dirname "${BASH_SOURCE[0]}" )" )" && pwd )"
 rm -Rf ${DIR}/edx-platform/node_modules
 
-docker-compose exec lms bash -c 'rm -Rf /edx/app/edxapp/edx-platform/node_modules'
+#docker-compose exec lms bash -c 'rm -Rf /edx/app/edxapp/edx-platform/node_modules'
 docker-compose exec lms bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform && NO_PYTHON_UNINSTALL=1 paver install_prereqs'
 
 #Installing prereqs crashes the process
